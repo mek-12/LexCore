ff6ec4190f48180df02a23c2e59fe8eb3ac13eb3
mek-12 <mek.krbg04@gmail.com>
2025-04-23 23:44:29 +0300

    EntityFramework Code added

---
+++ LexHarvester/src/LexHarvester.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using LexHarvester.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LexHarvester.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<MevzuatDocument> MevzuatDocuments { get; set; }
+    public DbSet<IctihatDocument> IctihatDocuments { get; set; }
+    public DbSet<MevzuatType> MevzuatTypes { get; set; }
+    public DbSet<IctihatType> IctihatTypes { get; set; }
+    public DbSet<RequestEndpointConfig> RequestEndpointConfigs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<MevzuatDocument>().HasIndex(x => x.MevzuatId).IsUnique();
+        modelBuilder.Entity<IctihatDocument>().HasIndex(x => x.DocumentId).IsUnique();
+        modelBuilder.Entity<MevzuatType>().HasIndex(x => x.MevzuatTurId).IsUnique();
+        modelBuilder.Entity<IctihatType>().HasIndex(x => x.Name).IsUnique();
+    }
+}
--- LexHarvester/src/LexHarvester.Persistence/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace LexHarvester.Persistence;
-
-public class Class1
-{
-
-}
