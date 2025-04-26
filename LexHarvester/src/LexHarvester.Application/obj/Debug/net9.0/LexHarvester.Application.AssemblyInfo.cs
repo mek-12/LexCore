4d783056498113916f434b577ebb37a0ea439d9b
mek-12 <mek.krbg04@gmail.com>
2025-04-26 12:54:17 +0300

    Program.cs configured for code first migration

--- LexHarvester/src/LexHarvester.Application/obj/Debug/net9.0/LexHarvester.Application.AssemblyInfo.cs
+++ LexHarvester/src/LexHarvester.Application/obj/Debug/net9.0/LexHarvester.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LexHarvester.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff6ec4190f48180df02a23c2e59fe8eb3ac13eb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+314f911d0e37db74ed0b59b14f8aee24d2c043ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("LexHarvester.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LexHarvester.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
