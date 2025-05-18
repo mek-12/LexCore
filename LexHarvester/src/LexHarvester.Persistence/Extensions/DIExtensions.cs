6f28eaad91096f246471898fb310506819fa3871
mek-12 <mek.krbg04@gmail.com>
2025-05-19 01:30:29 +0300

    navend core added

--- LexHarvester/src/LexHarvester.Persistence/Extensions/DIExtensions.cs
+++ LexHarvester/src/LexHarvester.Persistence/Extensions/DIExtensions.cs
@@ -1,12 +1,8 @@
-using LexHarvester.Persistence.Repositories;
-using LexHarvester.Persistence.UOW;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LexHarvester.Persistence.Extensions;
 public static class DIExtension {
     public static IServiceCollection AddRepositories(this IServiceCollection services, Action<IServiceCollection> action) {
-        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-        services.AddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
         action(services);
         return services;
     }
