c656efbc52ee8cebbbe7bd6bb38603104ff40f86
mek-12 <mek.krbg04@gmail.com>
2025-05-25 22:24:22 +0300

    legislationType sync mechanism executed

--- LexHarvester/src/LexHarvester.Domain/DTOs/LegislationTypeDto.cs
+++ LexHarvester/src/LexHarvester.Domain/DTOs/LegislationTypeDto.cs
@@ -1,11 +1,18 @@
+using Newtonsoft.Json;
+
 namespace LexHarvester.Domain.DTOs;
 
 public class LegislationTypeDto
 {
-    public int LegislationTypeId { get; set; } 
-    public string LegislationTypeCode { get; set; } = string.Empty;
-    public string LegislationTypeTitle { get; set; } = string.Empty;
-    public int OrderNumber { get; set; } 
+    [JsonProperty("mevzuatTurId")]
+    public int LegislationTypeId { get; set; } // MevzuatTurId
+    [JsonProperty("mevzuatTur")]
+    public string LegislationTypeCode { get; set; } = string.Empty;// MevzuatTur
+    [JsonProperty("mevzuatTurAdi")]
+    public string LegislationTypeTitle { get; set; } = string.Empty;// MevzuatTurAdi
+    [JsonProperty("siraNo")]
+    public int OrderNumber { get; set; } // SiraNo
+    [JsonProperty("desktopUpdatedDate")]
     public DateTime? LastOperationDate { get; set; }
     public int Count { get; set; }
 }
--- LexHarvester/src/LexHarvester.Infrastructure/Providers/LegislationTypeProvider.cs
+++ LexHarvester/src/LexHarvester.Infrastructure/Providers/LegislationTypeProvider.cs
@@ -14,15 +14,22 @@ public class LegislationTypeProvider : ILegislationTypeProvider
     }
     public async Task<LegislationTypeResponse> GetAsync(LegislationTypeRequest legislationTypeRequest)
     {
-        var request = new HttpRequestMessage(HttpMethod.Post, "emsal-karar/getBirimler")
+        try
         {
-            Content = new StringContent(JsonConvert.SerializeObject(legislationTypeRequest), System.Text.Encoding.UTF8, "application/json")
-        };
-        var response = await _httpClient.SendAsync(request, CancellationToken.None);
-        response.EnsureSuccessStatusCode();
+            var request = new HttpRequestMessage(HttpMethod.Post, "mevzuat/mevzuatTypes")
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(legislationTypeRequest), System.Text.Encoding.UTF8, "application/json")
+            };
+            var response = await _httpClient.SendAsync(request, CancellationToken.None);
+            response.EnsureSuccessStatusCode();
 
-        var content = await response.Content.ReadAsStringAsync(CancellationToken.None);
-        return JsonConvert.DeserializeObject<LegislationTypeResponse>(content)
-            ?? throw new InvalidOperationException("Failed to deserialize LegislationTypeDto from response content.");
+            var content = await response.Content.ReadAsStringAsync(CancellationToken.None);
+            var result = JsonConvert.DeserializeObject<LegislationTypeResponse>(content);
+            return result ?? throw new InvalidOperationException("Failed to deserialize LegislationTypeDto from response content.");
+        }
+        catch (System.Exception ex)
+        {
+            throw ex;
+        }
     }
 }
